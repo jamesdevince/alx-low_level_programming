24231b1985877d2a94426139e8527aed88add9da
james <jamesdevince@gmail.com>
2022-09-09 23:03:11 -0700

    hello printf

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Function starts
+ *
+ *   *
+ *
+ *    * Return: Function closes
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
